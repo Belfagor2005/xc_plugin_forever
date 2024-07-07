328c193edf0f33e58f106183b6a8726902d98ef9
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-07 15:23:23 +0200

    test Upgrade

--- usr/lib/enigma2/python/Plugins/Extensions/XCplugin/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/XCplugin/Update.py
@@ -8,53 +8,23 @@ print("Update.py")
 
 def upd_done():
     from os import popen, system
-    cmd01 = "wget --no-cache --no-dns-cache http://patbuweb.com/xcplugin/xcforever.tar -O /tmp/xcforever.tar --post-data='action=purge';tar -xvf /tmp/xcforever.tar -C /"
-    cmd02 = "wget --no-check-certificate --no-cache --no-dns-cache -U 'Enigma2 - xcforever Plugin' -c 'http://patbuweb.com/xcplugin/xcforever.tar' -O '/tmp/xcforever.tar' --post-data='action=purge';tar -xvf /tmp/xcforever.tar -C /"
-    cmd22 = 'find /usr/bin -name "wget"'
-    res = popen(cmd22).read()
-    if 'wget' not in res.lower():
-        if os.path.exists('/etc/opkg'):
-            cmd23 = 'opkg update && opkg install wget'
-        else:
-            cmd23 = 'apt-get update && apt-get install wget'
-        popen(cmd23)
-    try:
-        popen(cmd02)
-    except:
-        popen(cmd01)
-    system('rm -rf /tmp/xcforever.tar')
+    installUrl = 'https://raw.githubusercontent.com/Belfagor2005/xc_plugin_forever/main/installer.sh'
+    cmd00 = 'wget -q "--no-check-certificate" ' + installUrl + ' -O - | /bin/sh'
+
+    # cmd01 = "wget --no-cache --no-dns-cache http://patbuweb.com/xcplugin/xcforever.tar -O /tmp/xcforever.tar --post-data='action=purge';tar -xvf /tmp/xcforever.tar -C /"
+    # cmd02 = "wget --no-check-certificate --no-cache --no-dns-cache -U 'Enigma2 - xcforever Plugin' -c 'http://patbuweb.com/xcplugin/xcforever.tar' -O '/tmp/xcforever.tar' --post-data='action=purge';tar -xvf /tmp/xcforever.tar -C /"
+    # cmd22 = 'find /usr/bin -name "wget"'
+    # res = popen(cmd22).read()
+    # if 'wget' not in res.lower():
+        # if os.path.exists('/etc/opkg'):
+            # cmd23 = 'opkg update && opkg install wget'
+        # else:
+            # cmd23 = 'apt-get update && apt-get install wget'
+        # popen(cmd23)
+    # try:
+        # popen(cmd02)
+    # except:
+        # popen(cmd01)
+    popen(cmd00)
+    # system('rm -rf /tmp/xcforever.tar')
     return
-
-
-'''
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import sys, os
-PY3 = sys.version_info.major >= 3
-print("Update.py")
-
-
-def upd_done():
-    from os import popen, system
-    cmd01 = "wget http://patbuweb.com/xcplugin/xcforever.tar -O /tmp/xcforever.tar ; tar -xvf /tmp/xcforever.tar -C /"
-    cmd02 = "wget --no-check-certificate -U 'Enigma2 - xcforever Plugin' -c 'http://patbuweb.com/xcplugin/xcforever.tar' -O '/tmp/xcforever.tar'; tar -xvf /tmp/xcforever.tar -C /"
-    cmd22 = 'find /usr/bin -name "wget"'
-    res = popen(cmd22).read()
-    if 'wget' not in res.lower():
-        if os.path.exists('/etc/opkg'):
-            cmd23 = 'opkg update && opkg install wget'
-        else:
-            cmd23 = 'apt-get update && apt-get install wget'
-        popen(cmd23)
-    try:
-        popen(cmd02)
-    except:
-        popen(cmd01)
-    system('rm -rf /tmp/xcforever.tar')
-    return
-'''
-
-
-
-
