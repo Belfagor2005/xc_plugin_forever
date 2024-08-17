ef47b7977f5cf527e3b2e85d5ad9087111c53247
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-08-17 13:27:05 +0200

    Update Console.py

--- usr/lib/enigma2/python/Plugins/Extensions/XCplugin/Console.py
+++ usr/lib/enigma2/python/Plugins/Extensions/XCplugin/Console.py
@@ -60,20 +60,20 @@ class Console(Screen):
             self.skinName = [skin, 'Console']
         self.errorOcurred = False
         self['text'] = ScrollLabel('')
-        self['key_red'] = StaticText(_('Cancel'))
-        self['key_green'] = StaticText(_('Hide'))
+        # self['key_red'] = StaticText('Cancel')
+        # self['key_green'] = StaticText('Hide')
         self["actions"] = ActionMap(["WizardActions", "DirectionActions", 'ColorActions'],
-            {
-            "ok": self.cancel,
-            "up": self["text"].pageUp,
-            "down": self["text"].pageDown,
-            "red": self.cancel,
-            "green": self.toggleHideShow,
-            "blue": self.restartenigma,
-            "exit": self.cancel,
-            }, -1)
+                                    {
+                                    "ok": self.cancel,
+                                    "up": self["text"].pageUp,
+                                    "down": self["text"].pageDown,
+                                    "red": self.cancel,
+                                    "green": self.toggleHideShow,
+                                    "blue": self.restartenigma,
+                                    "cancel": self.cancel,
+                                    }, -1)
         self.cmdlist = isinstance(cmdlist, list) and cmdlist or [cmdlist]
-        self.newtitle = title == 'Console' and _('Console') or title
+        self.newtitle = title == 'Console' and ('Console') or title
         self.cancel_msg = None
         self.onShown.append(self.updateTitle)
         self.container = eConsoleAppContainer()
@@ -92,7 +92,7 @@ class Console(Screen):
 
     def startRun(self):
         if self.showStartStopText:
-            self['text'].setText(_('Execution progress') + '\n\n')
+            self['text'].setText('Execution progress\n\n')
         print('[Console] executing in run', self.run, ' the command:', self.cmdlist[self.run])
         if self.container.execute(self.cmdlist[self.run]):
             self.runFinished(-1)
@@ -115,15 +115,15 @@ class Console(Screen):
             if self.cancel_msg:
                 self.cancel_msg.close()
             if self.showStartStopText:
-                self['text'].appendText(_('Execution finished!!'))
+                self['text'].appendText('Execution finished!!')
             if self.finishedCallback is not None:
                 self.finishedCallback()
             if not self.errorOcurred and self.closeOnSuccess:
                 self.closeConsole()
             else:
-                self['text'].appendText(_('\nPress OK or Exit to abort!'))
-                self['key_red'].setText(_('Exit'))
-                self['key_green'].setText('')
+                self['text'].appendText('\nPress OK or Exit to abort!')
+                # self['key_red'].setText('Exit')
+                # self['key_green'].setText('')
 
     def toggleHideShow(self):
         if self.finished:
@@ -137,7 +137,7 @@ class Console(Screen):
         if self.finished:
             self.closeConsole()
         else:
-            self.cancel_msg = self.session.openWithCallback(self.cancelCallback, MessageBox, _('Cancel execution?'), type=MessageBox.TYPE_YESNO, default=False)
+            self.cancel_msg = self.session.openWithCallback(self.cancelCallback, MessageBox, 'Cancel execution?', type=MessageBox.TYPE_YESNO, default=False)
 
     def cancelCallback(self, ret=None):
         self.cancel_msg = None
