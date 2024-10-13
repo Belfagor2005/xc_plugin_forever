96576f9e4a11113561ab4efbe3d08e9c153b14fd
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-10-13 14:45:21 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/xc_plugin_forever/main/installer.sh -O - | /bin/sh
 ## Only This 2 lines to edit with new version ######
-version='3.3'
-changelog='Rewrite code source for channel\nFix poster Show'
+version='3.4'
+changelog='Major Fix\nFix Global Name'
 ##
 TMPPATH=/tmp/XCplugin-main
 FILEPATH=/tmp/main.tar.gz
