88719f9d589e06eea2690c072b0ee8d6b934dd1b
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-11-15 20:09:15 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/xc_plugin_forever/main/installer.sh -O - | /bin/sh
-# # exec > >(tee -a /tmp/XCplugin_debug.txt) 2>&1
-# # set -x
+## setup command=wget -q --no-check-certificate "https://raw.githubusercontent.com/Belfagor2005/xc_plugin_forever/main/installer.sh?inline=false" -O - | /bin/sh
+
 ## Only This 2 lines to edit with new version ######
 version='3.6'
 changelog='BACK TO CATEGORY LIST\nFix DreamOS System\nEPG Fixed\nAll code unnecessary removed\nAdd Module import'
