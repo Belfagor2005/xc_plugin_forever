9d881569aba74c6b026bd1d1955a400ff894554b
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-07-26 11:51:57 +0200

    re factory

--- usr/lib/enigma2/python/Plugins/Extensions/XCplugin/addons/downloader2.py
+++ usr/lib/enigma2/python/Plugins/Extensions/XCplugin/addons/downloader2.py
@@ -1,99 +1,104 @@
-#!/usr/bin/python
-# Code by mfaraj57 and RAED (c) 2018
-
-# python3
-from __future__ import print_function
-from . import _
+import os
+import ssl
+import threading
+import time
+from urllib.request import Request, urlopen
 from Components.ActionMap import ActionMap
 from Components.Label import Label
-from Screens.MessageBox import MessageBox
-from Screens.Screen import Screen
-from enigma import eTimer, eConsoleAppContainer, getDesktop
-from os import path as os_path
 from Components.ProgressBar import ProgressBar
-from Tools.Downloader import downloadWithProgress
-from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-import os
+from Screens.Screen import Screen
+from Screens.MessageBox import MessageBox
+from twisted.internet import reactor
+from .. import _
+
+SKIN_imagedownloadScreen = """
+<screen position="center,center" size="600,200" title="Download in corso">
+	<widget name="activityslider" position="10,30" size="580,20" />
+	<widget name="package" position="10,60" size="580,30" font="Regular;20" />
+	<widget name="status" position="10,100" size="580,60" font="Regular;18" />
+</screen>
+"""
 
 
-try:
-	from Components.HTMLComponent import *
-except:
-	print("KeyAdder: No HTMLComponent file found")
+class DownloadThread(threading.Thread):
+	def __init__(self, url, target, progress_callback, completion_callback, error_callback):
+		super().__init__()
+		self.url = url
+		self.target = target
+		self.progress_callback = progress_callback
+		self.completion_callback = completion_callback
+		self.error_callback = error_callback
+		self._stop_event = threading.Event()
+		self.start_time = time.time()
 
-try:
-	from Components.GUIComponent import *
-except:
-	print("KeyAdder: No GUIComponent file found")
-
-
-# imagedownloadScreen screen
-sz_w = getDesktop(0).size().width()
-
-if sz_w == 1280:
-	SKIN_imagedownloadScreen = """
-<screen name="imagedownloadScreen" position="center,center" size="560,155" title="Downloading image...">
-<widget name="activityslider" position="20,50" size="510,20" borderWidth="1" transparent="1" />
-<widget name="package" position="20,5" size="510,45" font="Regular;18" halign="center" valign="center" transparent="1" />
-<widget name="status" position="20,80" size="510,45" font="Regular;16" halign="center" valign="center" transparent="1" />
-</screen>"""
-
-else:
-	SKIN_imagedownloadScreen = """
-<screen name="imagedownloadScreen" position="center,center" size="805,232" title="Downloading image...">
-<widget name="activityslider" position="30,75" size="755,30" borderWidth="1" transparent="1" />
-<widget name="package" position="30,7" size="755,60" font="Regular;27" halign="center" valign="center" transparent="1" />
-<widget name="status" position="30,120" size="755,60" font="Regular;24" halign="center" valign="center" transparent="1" />
-</screen>"""
-
-# progress screen
-sz_w = getDesktop(0).size().width()
-
-if sz_w == 1280:
-	SKIN_Progress = """
-<screen position="350,250"  size="550,155" title="Command execution..." >
-<widget name="text" position="10,10"  size="550,130" font="Console;18" />
-<widget name="slider" position="0,142" size="550,15" borderWidth="1" transparent="1" />
-</screen>"""
-
-else:
-	SKIN_Progress = """
-<screen position="500,430"  size="850,200" title="Command execution..." >
-<widget name="text" position="20,20"  size="850,160" font="Console;24" />
-<widget name="slider" position="0,185" size="850,20" borderWidth="1" transparent="1" />
-</screen>"""
-
-
-def log(label, data):
-	data = str(data)
-	open("/tmp/KeyAdder.log", "a").write("\n" + label + ":>" + data)
-
-
-def getversioninfo():
-	currversion = '1.0'
-	version_file = resolveFilename(SCOPE_PLUGINS, "Extensions/KeyAdder/tools/version")
-	if os.path.exists(version_file):
+	def run(self):
 		try:
-			fp = open(version_file, 'r').readlines()
-			for line in fp:
-				if 'version' in line:
-					currversion = line.split('=')[1].strip()
-		except:
-			pass
-	return (currversion)
+			target_dir = os.path.dirname(self.target)
+			if not os.path.exists(target_dir):
+				os.makedirs(target_dir)
+
+			req = Request(self.url)
+			req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
+
+			try:
+				context = ssl._create_unverified_context()
+				response = urlopen(req, context=context, timeout=30)
+			except:
+				response = urlopen(req, timeout=30)
+
+			total_size = int(response.headers.get('Content-Length', 0))
+			block_size = 1024 * 8
+			downloaded = 0
+
+			with open(self.target, 'wb') as f:
+				while True:
+					if self._stop_event.is_set():
+						f.close()
+						try:
+							os.remove(self.target)
+						except:
+							pass
+						return
+
+					buffer = response.read(block_size)
+					if not buffer:
+						break
+
+					downloaded += len(buffer)
+					f.write(buffer)
+
+					elapsed = time.time() - self.start_time
+					speed = downloaded / elapsed / 1024 if elapsed > 0 else 0  # KB/s
+					remaining = (total_size - downloaded) / (speed * 1024) if speed > 0 else 0
+
+					self.progress_callback(downloaded, total_size, speed, remaining)
+
+			self.completion_callback()
+
+		except Exception as e:
+			self.error_callback(str(e))
+
+	def stop(self):
+		self._stop_event.set()
 
 
 class imagedownloadScreen(Screen):
 	def __init__(self, session, name='', target='', url=''):
 		Screen.__init__(self, session)
 		self.skin = SKIN_imagedownloadScreen
+
 		self.target = target
 		self.name = name
 		self.url = url
+
+		self.downloading = False
+		self.success = False
+		self.error_message = ""
+
 		self["activityslider"] = ProgressBar()
 		self["activityslider"].setRange((0, 100))
 		self["activityslider"].setValue(0)
-		self["status"] = Label(_("Downloading, please wait..."))
+		self["status"] = Label(_("Preparando il download..."))
 		self["package"] = Label(name)
 
 		self["actions"] = ActionMap(
@@ -104,101 +109,110 @@ class imagedownloadScreen(Screen):
 			},
 			-1
 		)
-		self.downloading = False
-		self.downloader = None
-		self.setTitle(_('Connecting') + '...')
-		self.timer = eTimer()
-		try:
-			self.timer.callback.append(self.startDownload)
-		except AttributeError:
-			self.timer_conn = self.timer.timeout.connect(self.startDownload)
-		self.timer.start(5000, 1)
+
+		self.onLayoutFinish.append(self.startDownload)
 
 	def startDownload(self):
-		try:
-			self.timer.stop()
-			del self.timer
-		except Exception:
-			pass
+		"""Avvia il processo di download in un thread separato"""
 		self.downloading = True
-		self['status'].setText(_('Connecting to server...'))
+		self.setTitle(_("Download in corso"))
+		self["status"].setText(_("Connessione al server..."))
 
-		# Use downloadWithProgress async downloader
-		self.downloader = downloadWithProgress(self.url, self.target)
-		self.downloader.addProgress(self.progress)
-		d = self.downloader.start()
-		d.addCallback(self.responseCompleted)
-		d.addErrback(self.responseFailed)
+		self.download_thread = DownloadThread(
+			self.url,
+			self.target,
+			self.updateProgress,
+			self.downloadCompleted,
+			self.downloadFailed
+		)
+		self.download_thread.start()
 
-	def progress(self, current, total):
-		# Update progress bar and labels
-		if total == 0:
-			percent = 0
-		else:
-			percent = int(100 * current / float(total))
+	def updateProgress(self, current, total, speed, remaining):
+		reactor.callFromThread(self._updateProgressGUI, current, total, speed, remaining)
 
-		self['activityslider'].setValue(percent)
-		info = _('Downloading') + ' ' + '%d of %d kBytes (%.2f%%)' % (current // 1024, total // 1024, 100 * current / float(total))
-		self['package'].setText(self.name)
-		self['status'].setText(info)
-		self.setTitle(_('Downloading') + ' ' + str(percent) + '%...')
+	def downloadCompleted(self):
+		reactor.callFromThread(self._downloadCompletedGUI)
 
-	def responseCompleted(self, data=None):
-		# Called when download finished successfully
-		print('[downloader] Download succeeded.')
-		info = _('Download completed successfully.\nPress OK to exit.')
-		self['status'].setText(info)
-		self.setTitle(_('Download completed successfully.'))
+	def downloadFailed(self, error_message):
+		reactor.callFromThread(self._downloadFailedGUI, error_message)
+
+	def _updateProgressGUI(self, current, total, speed, remaining):
+		"""Aggiorna gli elementi GUI (chiamato nel thread principale)"""
+		if total > 0:
+			percent = int(100 * current / total)
+			self["activityslider"].setValue(percent)
+
+			status = _("Scaricamento: %d%% - Velocità: %.1f KB/s - Tempo rimanente: %d sec") % (
+				percent, speed, remaining
+			)
+			self["status"].setText(status)
+			self.setTitle(_("Download: %d%%") % percent)
+
+	def _downloadCompletedGUI(self):
+		"""Aggiorna la GUI per download completato (thread principale)"""
 		self.downloading = False
 		self.success = True
-		self.instance.show()
+		self["status"].setText(_("Download completato con successo!"))
+		self["activityslider"].setValue(100)
+		self.setTitle(_("Download completato"))
 
-	def responseFailed(self, failure_instance=None, error_message=''):
-		# Called when download fails
-		print('[downloader] Download failed.')
-		self.error_message = error_message
-		if error_message == '' and failure_instance is not None:
-			self.error_message = failure_instance.getErrorMessage()
-		info = self.error_message
-		self['status'].setText(info)
-		self.setTitle(_('Download failed. Press OK to exit.'))
+	def _downloadFailedGUI(self, error_message):
+		"""Aggiorna la GUI per errore (thread principale)"""
 		self.downloading = False
 		self.success = False
-								
-		self.instance.show()
-		self.remove_target()
+		self.error_message = error_message
+
+		if "401" in self.error_message:
+			self.error_message = _("Accesso non autorizzato (401)")
+		elif "404" in self.error_message:
+			self.error_message = _("Risorsa non trovata (404)")
+		elif "timed out" in self.error_message:
+			self.error_message = _("Timeout della connessione")
+
+		self["status"].setText(self.error_message)
+		self.setTitle(_("Download fallito"))
+
+		if os.path.exists(self.target):
+			try:
+				os.remove(self.target)
+			except:
+				pass
 
 	def dexit(self):
+		"""Gestisce l'uscita dallo schermo"""
 		if self.downloading:
-			self.session.openWithCallback(self.abort, MessageBox, _('Are you sure to stop download?'), MessageBox.TYPE_YESNO)
+			self.session.openWithCallback(
+				self.confirmAbort,
+				MessageBox,
+				_("Vuoi davvero interrompere il download?"),
+				MessageBox.TYPE_YESNO
+			)
 		else:
+			self.close(self.success)
+
+	def confirmAbort(self, answer):
+		"""Conferma l'interruzione del download"""
+		if answer:
+			if self.download_thread:
+				try:
+					self.download_thread.stop()
+				except:
+					pass
+
+			if os.path.exists(self.target):
+				try:
+					os.remove(self.target)
+				except:
+					pass
+
 			self.close(False)
 
-	def remove_target(self):
-		# Remove partially downloaded target file
-		try:
-			if os_path.exists(self.target):
-				os.remove(self.target)
-		except Exception:
-			pass
+	def close(self, success=False):
+		"""Chiude lo schermo"""
+		if self.download_thread and self.download_thread.is_alive():
+			try:
+				self.download_thread.stop()
+			except:
+				pass
 
-	def abort(self, answer=True):
-		if not answer:
-			return
-		if not self.downloading:
-			try:
-				if os_path.exists('/tmp/download_install.log'):
-					os.remove('/tmp/download_install.log')
-			except Exception:
-				pass
-			self.close(False)
-		elif self.downloader is not None:
-			self.downloader.stop()
-			self['status'].setText(_('Aborting...'))
-			self.remove_target()
-			try:
-				self.close(False)
-			except Exception:
-				pass
-		else:
-			self.close(False)
+		Screen.close(self, success)
